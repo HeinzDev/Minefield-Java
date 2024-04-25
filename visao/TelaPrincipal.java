0652dc07af41960cd4548354c0681279ac20aef2
Enzo <102617566+HeinzDev@users.noreply.github.com>
2024-04-24 21:16:36 -0300

    First commit

---
+++ visao/TelaPrincipal.java
@@ -0,0 +1,28 @@
+package com.enzo.cm.visao;
+
+import javax.swing.JFrame;
+
+import com.enzo.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class TelaPrincipal extends JFrame {
+
+	public TelaPrincipal() {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		
+		add(new PainelTabuleiro(tabuleiro));
+		
+		
+		setTitle("Campo Minado");
+		setSize(690, 438);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		new TelaPrincipal();
+	}
+}
